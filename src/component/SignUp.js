b826c66272eb6a7d52f59104c3d37380919b951b
Juan Felipe Ortiz <jfelipeon@outlook.com>
2018-10-02 14:54:28 -0500

    Primer Commit Proyecto COSW contiene
    -Dependecias de Materialiu core, icon, moment
    
    -Vista de entrtada - no terminada - se debe terminar
    
    -

---
+++ src/component/SignUp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './SignUp.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import moment from "moment";
+
+
+export class SignUp extends React.Component{
+
+
+    render(){
+
+        constructor(props) {
+            super(props);
+            this.state = {text: '', priority: 0, dueDate: moment()};
+        }
+
+
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <main className="layout">
+                    <Paper className="paper">
+
+                    </Paper>
+                </main>
+            </React.Fragment>
+
+        );
+    }
+
+}
